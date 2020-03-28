02ada314839aa751642a4fe9e54250c59217ed20
Btriordan <62084562+Btriordan@users.noreply.github.com>
2020-03-28 14:05:05 -0700

    Delete Loops.py

--- Loops.py
+++
@@ -1,6 +0,0 @@
-message_to_candidate = (
-    f"You received {candidate_votes} number of votes. "
-    f"The total number of votes in the election was {total_votes}. "
-    f"You received {candidate_votes / total_votes * 100}% of the total votes.")
-
-print(message_to_candidate)
